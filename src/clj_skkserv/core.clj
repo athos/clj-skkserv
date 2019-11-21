9623207ec2a588c6f6b0e9e59ff26ff45b3ef8a7
Shogo Ohta <athos0220@gmail.com>
2019-11-21 09:42:01 +0900

    Reimplement server based on Reader/Writer abstraction

--- src/clj_skkserv/core.clj
+++ src/clj_skkserv/core.clj
@@ -1,43 +1,40 @@
 (ns clj-skkserv.core
   (:refer-clojure :exclude [send])
-  (:import [java.io OutputStream InputStream]
-           [java.net ServerSocket]))
+  (:import [java.io
+            BufferedReader
+            InputStreamReader
+            OutputStreamWriter
+            Reader
+            Writer]
+           [java.net ServerSocket]
+           [java.nio CharBuffer]))
 
-(defn send-raw [^OutputStream out ^bytes bytes]
-  (.write out bytes)
+(defn send [^Writer out ^String s]
+  (.append out s)
   (.flush out))
 
-(defn send [out ^String s]
-  (send-raw out (.getBytes s "EUC-JP")))
-
-(defn read-until-space [^InputStream in ^bytes buf]
-  (loop [offset 0]
-    (let [b (.read in)]
-      (if (or (= b 32) (= b -1))
-        (String. buf 0 offset "EUC-JP")
-        (do (aset buf offset (unchecked-byte b))
-            (recur (inc offset)))))))
+(defn read-until-space [^Reader in]
+  (loop [buf (CharBuffer/allocate 128)]
+    (let [c (.read in)]
+      (if (or (= c 32) (= c -1))
+        (-> (.flip buf) (.toString))
+        (recur (.put buf (char c)))))))
 
 (defn -main []
   (let [server (ServerSocket. 8080)]
     (with-open [socket (.accept server)]
-      (let [in (.getInputStream socket)
-            out (.getOutputStream socket)
-            buf (byte-array 256)]
+      (let [in (BufferedReader. (InputStreamReader. (.getInputStream socket) "EUC-JP"))
+            out (OutputStreamWriter. (.getOutputStream socket) "EUC-JP")]
         (loop []
-          (let [b (.read in)]
-            (when (nat-int? b)
-              (print (char b))
-              (flush)
-              (let [disconnect? (case b
+          (let [c (.read in)]
+            (when (nat-int? c)
+              (let [disconnect? (case c
                                   48 true
-                                  49 (let [s (read-until-space in buf)]
-                                       (println :conversion s)
+                                  49 (let [s (read-until-space in)]
                                        (send out "1/ほげ/ふが/\n"))
-                                  50 (send-raw out (.getBytes "clj-skkserv.0.1 "))
-                                  51 (send-raw out (.getBytes "127.0.0.1:8080 "))
-                                  52 (let [s (read-until-space in buf)]
-                                       (println :complement s)
+                                  50 (send out "clj-skkserv.0.1 ")
+                                  51 (send out "127.0.0.1:8080 ")
+                                  52 (let [s (read-until-space in)]
                                        (send out "4\n")))]
                 (when-not disconnect?
                   (recur))))))))))
