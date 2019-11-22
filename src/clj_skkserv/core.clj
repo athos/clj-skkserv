4ff2c6d589444f69f8ca500b9c3e1fd8814a8a82
Shogo Ohta <athos0220@gmail.com>
2019-11-22 10:26:17 +0900

    Accept handler impl from outside

--- src/clj_skkserv/core.clj
+++ src/clj_skkserv/core.clj
@@ -9,32 +9,84 @@
            [java.net ServerSocket]
            [java.nio CharBuffer]))
 
-(defn send [^Writer out ^String s]
+(defn- send [^Writer out ^String s]
   (.append out s)
   (.flush out))
 
-(defn read-until-space [^Reader in]
+(defmacro thread [& body]
+  `(doto (Thread. (fn [] ~@body))
+     (.start)))
+
+(defn- read-until-space [^Reader in]
   (loop [buf (CharBuffer/allocate 128)]
     (let [c (.read in)]
       (if (or (= c 32) (= c -1))
         (-> (.flip buf) (.toString))
         (recur (.put buf (char c)))))))
 
+(defn- handle-request [handler ^Reader in ^Writer out opts]
+  (loop [type nil]
+    (when type
+      (let [content (when (or (= type :conversion) (= type :completion))
+                      (read-until-space in))]
+        (prn :content content)
+        (handler type content out)))
+    (let [c (.read in)]
+      (when (>= c 0)
+        (case c
+          48 nil
+          49 (recur :conversion)
+          50 (recur :version)
+          51 (recur :name)
+          52 (recur :completion)
+          (recur nil))))))
+
+(defn wrap-standard [handler opts]
+  (fn [type content out]
+    (case type
+      :version (send out "clj-skkserv.0.1 ")
+      :name (send out "127.0.0.1:8080 ")
+      (handler type content out))))
+
+(defn start-server
+  ([handler] (start-server handler {}))
+  ([handler {:keys [port] :or {port 1178} :as opts}]
+   (println "starting server ...")
+   (let [socket (ServerSocket. port)]
+     (loop []
+       (let [conn (.accept socket)
+             in (-> (.getInputStream conn)
+                    (InputStreamReader. "EUC-JP")
+                    (BufferedReader.))
+             out (-> (.getOutputStream conn) (OutputStreamWriter. "EUC-JP"))
+             handler' (-> handler (wrap-standard opts))]
+         (println "accepted new connection")
+         (when-not (.isClosed socket)
+           (thread
+             (try
+               (handle-request handler' in out opts)
+               (finally
+                 (.close conn)
+                 (println "disconnected connection"))))
+           (recur))))
+     socket)))
+
+(defn hiragana->katakana [s]
+  (let [re #"[\u3040-\u309F]"]
+    (when (re-find re s)
+      (-> s
+          (clojure.string/replace re #(str (char (+ (int (first %)) 96))))
+          (clojure.string/replace #"[a-z]$" "")))))
+
 (defn -main []
-  (let [server (ServerSocket. 8080)]
-    (with-open [socket (.accept server)]
-      (let [in (BufferedReader. (InputStreamReader. (.getInputStream socket) "EUC-JP"))
-            out (OutputStreamWriter. (.getOutputStream socket) "EUC-JP")]
-        (loop []
-          (let [c (.read in)]
-            (when (nat-int? c)
-              (let [disconnect? (case c
-                                  48 true
-                                  49 (let [s (read-until-space in)]
-                                       (send out "1/ほげ/ふが/\n"))
-                                  50 (send out "clj-skkserv.0.1 ")
-                                  51 (send out "127.0.0.1:8080 ")
-                                  52 (let [s (read-until-space in)]
-                                       (send out "4\n")))]
-                (when-not disconnect?
-                  (recur))))))))))
+  (letfn [(conv [s]
+            (case s
+              "/time" (str (java.util.Date.))))
+          (handler [type content out]
+            (case type
+              :conversion (send out
+                                (if-let [katakana (hiragana->katakana content)]
+                                  (format "1/%s/\n" katakana)
+                                  "1\n"))
+              :completion (send out "4\n")))]
+    (start-server handler)))
