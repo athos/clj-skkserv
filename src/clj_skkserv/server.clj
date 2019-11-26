589b959129482c2218c82b8ae58f1bea32c7874f
Shogo Ohta <athos0220@gmail.com>
2019-11-26 22:14:45 +0900

    Return real address:port for host requests

--- src/clj_skkserv/server.clj
+++ src/clj_skkserv/server.clj
@@ -22,31 +22,40 @@
         (-> (.flip buf) (.toString))
         (recur (.put buf (char c)))))))
 
-(defn- handle [handler ^Reader in ^Writer out opts]
-  (loop [type nil]
-    (when type
-      (let [res (res/make-response type out)]
-        (case type
-          (:conversion :completion)
-          (try
-            (let [content (read-until-space in)]
-              (prn :content content)
-              (res/respond res (handler type content)))
-            (catch Throwable _
-              (when-not (res/responded? res)
-                (-> (res/emit res "0") (res/flush res)))))
-          :version (res/emit res "clj-skkserv.0.1 ")
-          :host (res/emit res "127.0.0.1:1178 ")
-          nil)))
-    (let [c (.read in)]
-      (when (>= c 0)
-        (case c
-          48 nil
-          49 (recur :conversion)
-          50 (recur :version)
-          51 (recur :host)
-          52 (recur :completion)
-          (recur nil))))))
+(defn- emit [res content]
+  (-> res
+      (res/emit content)
+      (res/flush)))
+
+(defn- handle-fn
+  [handler {:keys [address port] :or {address "localhost" port 1178}}]
+  (let [version "clj-skkserv.0.1 "
+        host (str address \: port \space)]
+    (fn [^Reader in ^Writer out]
+      (loop [type nil]
+        (when type
+          (let [res (res/make-response type out)]
+            (case type
+              (:conversion :completion)
+              (try
+                (let [content (read-until-space in)]
+                  (prn :content content)
+                  (res/respond res (handler type content)))
+                (catch Throwable _
+                  (when-not (res/responded? res)
+                    (emit res "0"))))
+              :version (emit res version)
+              :host (emit res host)
+              nil)))
+        (let [c (.read in)]
+          (when (>= c 0)
+            (case c
+              48 nil
+              49 (recur :conversion)
+              50 (recur :version)
+              51 (recur :host)
+              52 (recur :completion)
+              (recur nil))))))))
 
 (defmacro thread [& body]
   `(doto (Thread. (fn [] ~@body))
@@ -55,7 +64,8 @@
 (defn start-server [handler {:keys [address port] :as opts}]
   (println "starting server ...")
   (let [address (InetAddress/getByName address)
-        socket (ServerSocket. port 0 address)]
+        socket (ServerSocket. port 0 address)
+        handle (handle-fn handler opts)]
     (loop []
       (let [conn (.accept socket)
             in (-> (.getInputStream conn)
@@ -66,7 +76,7 @@
         (when-not (.isClosed socket)
           (thread
             (try
-              (handle handler in out opts)
+              (handle in out)
               (finally
                 (.close conn)
                 (println "disconnected connection"))))
