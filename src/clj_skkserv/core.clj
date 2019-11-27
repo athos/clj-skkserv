de04abe23944fb7dd899aa8bee02c2815f285126
Shogo Ohta <athos0220@gmail.com>
2019-11-27 21:55:54 +0900

    Set default bind address

--- src/clj_skkserv/core.clj
+++ src/clj_skkserv/core.clj
@@ -3,6 +3,8 @@
 
 (defn start-server
   ([handler] (start-server handler {}))
-  ([handler {:keys [port] :as opts}]
-   (let [opts' (cond-> opts (nil? port) (assoc :port 1178))]
+  ([handler {:keys [address port] :as opts}]
+   (let [opts' (cond-> opts
+                 (nil? address) (assoc :address "127.0.0.1")
+                 (nil? port) (assoc :port 1178))]
      (server/start-server handler opts'))))
